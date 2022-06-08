e619a12d03a38ee44aa27292977c921fed9e3cd6
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-09 00:25:53 +0200

    update about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,21 +4,29 @@ import "./About.scss";
 const About = () => {
   return (
     <div className="about wrapper">
-      
       <h1 className="heading">About me</h1>
       <div className="about__container">
         <div className="img-container">
           <img src="./profile.jpg" alt="profile" />
         </div>
         <div className="about__text">
-          I'm a junior frontend developer, I'm able to work independently &
-          collaboratively in a team to deliver a great user experience and
-          satisfy the needs of my clients.
+          As a Frontend developer, I enjoy bridging the gap between engineering
+          and design — combining my technical knowledge with my keen eye for
+          design to create scalable and efficient applications while
+          providing engaging, pixel-perfect user experiences.
           <br />
-          I have a good design eye, with a meticulous attention to details & I'm
-          eager to find both a good and efficient way of solving problems.
+          I have a good experience, working as a freelance on various projects.
+       
           <br />
           Here are a few technologies I’ve been working with recently:
+          <ul className="about__list">
+            <li>JavaScript (ES6+) </li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>GraphQl</li>
+            <li>Next.js</li>
+            <li>Tailwindcss</li>
+          </ul>
         </div>
       </div>
     </div>
