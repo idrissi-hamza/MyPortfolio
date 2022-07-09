4df020316cec6580a0542dcc7133a1ac2aa37f83
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-07-09 21:29:58 +0100

    fix offset and padding for correct scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const context = useContext(globalContext);
   const { darkMode, open } = context.state;
   const links = [
-    { label: "Home", id: "/" },
+    { label: "Home", id: "/", offset: 1 },
     { label: "About", id: "about" },
     { label: "Portfolio", id: "portfolio" },
     { label: "Contact", id: "contact" },
@@ -38,10 +38,10 @@ const Navbar = () => {
           <Toggle />
         </div>
 
-{/* big screen navbar */}
+        {/* big screen navbar */}
 
         {!open && (
-          <nav className={`navbar__right  navbar-lg `} >
+          <nav className={`navbar__right  navbar-lg `}>
             <ul className="navbar__list">
               {links.map((link, i) => (
                 <li key={i}>
@@ -51,6 +51,7 @@ const Navbar = () => {
                     activeClass="active"
                     smooth
                     spy
+                    offset={link.offset || 50}
                   >
                     {link.label}
                   </Link>
@@ -63,12 +64,15 @@ const Navbar = () => {
 
       {/* small screen navbar */}
       {open && (
-        <nav className={`navbar__right navbar-sm`}  style={{
-          background: darkMode
-            ? "hsla(199, 24%, 28%, 0.9)"
-            : "hsla(214, 100%, 95%, 0.9)",
-          color: darkMode ? "#e6f1ff" : "#374E59",
-        }}>
+        <nav
+          className={`navbar__right navbar-sm`}
+          style={{
+            background: darkMode
+              ? "hsla(199, 24%, 28%, 0.9)"
+              : "hsla(214, 100%, 95%, 0.9)",
+            color: darkMode ? "#e6f1ff" : "#374E59",
+          }}
+        >
           <ul className="navbar__list">
             {links.map((link, i) => (
               <li key={i} onClick={() => context.dispatch({ type: "close" })}>
